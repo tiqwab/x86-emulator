813100409c7f5f1282df035a780d28ed2bab93fe
tiqwab <tiqwab.ch90@gmail.com>
2018-11-05 23:01:23 +0900

    Parse simple header of exe

--- emulator.go
+++ emulator.go
@@ -1,1 +1,72 @@
 package x86_emulator
+
+import (
+	"io"
+	"bufio"
+	)
+
+type word uint16
+
+type exe struct {
+	rawHeader []byte
+	header header
+}
+
+type header struct {
+	exSignature [2]byte
+	exHeaderSize word
+	exInitSS word
+	exInitSP word
+	exInitIP word
+	exInitCS word
+}
+
+func parseHeader(reader io.Reader) *header {
+	buf := make([]byte, 256)
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanBytes)
+
+	buf = parseBytes(2, sc)
+	exSignature := [2]byte{buf[0], buf[1]}
+
+	parseBytes(6, sc)
+
+	exHeaderSize := parseWord(sc)
+
+	parseBytes(4, sc)
+
+	exInitSS := parseWord(sc)
+	exInitSP := parseWord(sc)
+
+	parseBytes(2, sc)
+
+	exInitIP := parseWord(sc)
+	exInitCS := parseWord(sc)
+
+	return &header{
+		exSignature: exSignature,
+		exHeaderSize: exHeaderSize,
+		exInitSS: exInitSS,
+		exInitSP: exInitSP,
+		exInitIP: exInitIP,
+		exInitCS: exInitCS,
+	}
+}
+
+func parseBytes(n int, sc *bufio.Scanner) []byte {
+	buf := make([]byte, n)
+	for i := 0; i < n; i++ {
+		if b := sc.Scan(); b {
+			buf[i] = sc.Bytes()[0]
+		} else {
+			return nil
+		}
+	}
+	return buf
+}
+
+// assume little-endian
+func parseWord(sc *bufio.Scanner) word {
+	buf := parseBytes(2, sc)
+	return word(buf[1]) << 8 + word(buf[0])
+}
--- emulator_test.go
+++ emulator_test.go
@@ -1,7 +1,73 @@
 package x86_emulator
 
-import "testing"
+import (
+	"testing"
+	"bytes"
+	"io"
+)
+
+func rawHeader() []byte {
+	// 32 bytes
+	return []byte{
+		0x4d, 0x5a, 0x2b, 0x00, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0x01, 0xff, 0xff, 0x01, 0x00,
+		0x00, 0x10, 0x00, 0x00, 0x00, 0x01, 0x02, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+}
 
 func TestExample1(t *testing.T) {
 	t.Log("example1")
+}
+
+func TestParseHeaderSignature(t *testing.T) {
+	var reader io.Reader = bytes.NewReader(rawHeader())
+	actual := parseHeader(reader)
+	expected := [2]byte{'M', 'Z'}
+	if actual.exSignature != expected {
+		t.Errorf("expected %v but actual %v", expected, actual.exSignature)
+	}
+}
+
+func TestParseHeaderSize(t *testing.T) {
+	var reader io.Reader = bytes.NewReader(rawHeader())
+	actual := parseHeader(reader)
+	expected := word(2)
+	if actual.exHeaderSize != expected {
+		t.Errorf("expected %v but actual %v", expected, actual.exHeaderSize)
+	}
+}
+
+func TestParseHeaderInitSS(t *testing.T) {
+	var reader io.Reader = bytes.NewReader(rawHeader())
+	actual := parseHeader(reader)
+	expected := word(0x0001)
+	if actual.exInitSS != expected {
+		t.Errorf("expected %v but actual %v", expected, actual.exInitSS)
+	}
+}
+
+func TestParseHeaderInitSP(t *testing.T) {
+	var reader io.Reader = bytes.NewReader(rawHeader())
+	actual := parseHeader(reader)
+	expected := word(0x1000)
+	if actual.exInitSP != expected {
+		t.Errorf("expected %v but actual %v", expected, actual.exInitSP)
+	}
+}
+
+func TestParseHeaderInitIP(t *testing.T) {
+	var reader io.Reader = bytes.NewReader(rawHeader())
+	actual := parseHeader(reader)
+	expected := word(0x0100)
+	if actual.exInitIP != expected {
+		t.Errorf("expected %v but actual %v", expected, actual.exInitIP)
+	}
+}
+
+func TestParseHeaderInitCS(t *testing.T) {
+	var reader io.Reader = bytes.NewReader(rawHeader())
+	actual := parseHeader(reader)
+	expected := word(0x0002)
+	if actual.exInitCS != expected {
+		t.Errorf("expected %v but actual %v", expected, actual.exInitCS)
+	}
 }
